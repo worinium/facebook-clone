9f486a3447f760a4433947f329e60f23100308a7
Alapher W. Hearty <alapherwori8g5@gmail.com>
2020-09-22 04:21:52 +0100

    Major Commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <Toolbar/>
+    <DrawerLeft/>
+    <DrawerRight/>
+
+    <v-content>
+      <Feed/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from './components/ToolBar'
+import DrawerLeft from './components/DrawerLeft'
+import DrawerRight from './components/DrawerRight'
+import Feed from './components/Feed'
+
+export default {
+  name: 'App',
+  components: {
+    Toolbar,
+    DrawerLeft,
+    DrawerRight,
+    Feed
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css?family=Questrial");
+
+.questrial {
+  font-family: "Questrial";
+}
+</style>
